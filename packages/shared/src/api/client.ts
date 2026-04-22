e228a4533c1aea0475e4efc5c6d411042f79df80
Ramesh <rvrameshn@gmail.com>
2026-04-22 23:32:18 +0530

    "Interceptors has been added."

--- packages/shared/src/api/client.ts
+++ packages/shared/src/api/client.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
-export const apiClient = axios.create({
+const apiClient = axios.create({
   baseURL: 'https://dummyjson.com/'
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default apiClient;
